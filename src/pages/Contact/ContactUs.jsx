60c8da45971e2eba2031394cb35bc169c77e3ff3
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2024-04-12 07:59:37 +0300

    Removed unecessary element

--- src/pages/Contact/ContactUs.jsx
+++ src/pages/Contact/ContactUs.jsx
@@ -77,29 +77,26 @@ const ContactUs = () => {
 
                 <div className="contact_form">
                   <h2>We’d love to hear from you!</h2>
-                  <div className="contact_form">
-                    <h2>We’d love to hear from you!</h2>
-                    <form>
-                      <div className="form_group">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" id="name" name="name" required />
-                      </div>
-                      <div className="form_group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" required />
-                      </div>
-                      <div className="form_group">
-                        <label htmlFor="message">Message</label>
-                        <textarea
-                          id="message"
-                          name="message"
-                          rows="5"
-                          required
-                        ></textarea>
-                      </div>
-                      <button type="submit">Send Message</button>
-                    </form>
-                  </div>
+                  <form>
+                    <div className="form_group">
+                      <label htmlFor="name">Name</label>
+                      <input type="text" id="name" name="name" required />
+                    </div>
+                    <div className="form_group">
+                      <label htmlFor="email">Email</label>
+                      <input type="email" id="email" name="email" required />
+                    </div>
+                    <div className="form_group">
+                      <label htmlFor="message">Message</label>
+                      <textarea
+                        id="message"
+                        name="message"
+                        rows="5"
+                        required
+                      ></textarea>
+                    </div>
+                    <button type="submit">Send Message</button>
+                  </form>
                 </div>
               </div>
             </div>
