cd961ce74512451c15a96f1a5aed2b24a93ea669
Birhanu <birhanuworku531@gmail.com>
2024-06-13 18:30:51 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import Footer from './footer/footer';
 import About from './about/about';
+import Header from './component/header/Header'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Header</h1>
-      </header>
+      <main>
+      <Header />
+      </main>
       <main>
         <About />
       </main>
